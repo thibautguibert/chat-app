c71f5bb781a2dce5774583544137b2980c23b1d2
Thibaut <thibaut.guibert@edu.escp.eu>
2020-10-13 16:15:13 +0200

    creation of ContactList component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Carl Sims"
-        avatar="https://randomuser.me/api/portraits/men/31.jpg"
-        online />
-      <Contact
-        name="Eileen Rogers"
-        avatar="https://randomuser.me/api/portraits/women/11.jpg" />
-      <Contact
-        name="Anita Burton"
-        avatar="https://randomuser.me/api/portraits/women/84.jpg"
-        online />
+      <ContactList />
     </div>
   );
 }
