10fc6d32a45c041eb2a10723574dbed904138442
Thibaut <thibaut.guibert@edu.escp.eu>
2020-10-14 11:40:41 +0200

    added state on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,18 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Contact.css';
 
-const Contact = (props) => (
-    <div className="Contact">
-        <img className="avatar" src={props.avatar} alt={props.name} />
-        <div>
-            <h4 className="name">{props.name}</h4>
-            <div className="status">
-                <div className={props.online ? "status-online" : "status-offline"} />
-                <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online
+        };
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status"
+                        onClick={event => {
+                            const newStatus = !this.state.online;
+                            this.setState({ online: newStatus })
+                        }}>
+                        <div className={this.state.online ? "status-online" : "status-offline"} />
+                        <p className="status-text">{this.state.online ? "Online" : "Offline"}</p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 Contact.propTypes = {
     avatar: PropTypes.string.isRequired,
